5209a048a72a8d16ff3531d700b58a134103baf2
elizabethbright721 <elizabethbright721@gmail.com>
2023-09-09 10:15:53 -0400

    up to date frontend from main

--- src/Dashboards/PatientDashboard.js
+++ src/Dashboards/PatientDashboard.js
@@ -1,10 +1,11 @@
+
 import PatientJournalCards from "../Components/Patients/PatientJournalCards.js"
-// import TherapistDashboardNav from "../Components/Therapists/TherapistDashBoardNav.js"
+import TherapistDashboardNav from "../Components/TherapistDashboardNav.js"
 
 export default function PatientDashboard() {
   return (
     <div>
-       {/* <TherapistDashboardNav /> */}
+       <TherapistDashboardNav />
        <p>This is the patient dashboard</p>
         
     </div>
