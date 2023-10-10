8d24fc30462e91b709d982ccf5a072be973f9bbb
Wilton <wiltoncamilol@gmail.com>
2023-10-09 21:43:33 -0400

    Added target

--- src/Components/About.js
+++ src/Components/About.js
@@ -28,8 +28,8 @@ The Project-based curriculum provides hands-on experience with JavaScript, HTML,
          </div>
         <div className="flex space-x-5 items-baseline"> 
           <h1 className="mt-4 text-dark-blue text-2xl font-bold cursor-pointer">Elizabeth Bright</h1>
-          <a href="https://www.linkedin.com/in/elizabethbright721/"><SiLinkedin /></a>
-          <a href="https://github.com/elizabethbright721"><SiGithub /></a>
+          <a href="https://www.linkedin.com/in/elizabethbright721/" target="_blank" ><SiLinkedin /></a>
+          <a href="https://github.com/elizabethbright721" target="_blank"><SiGithub /></a>
         </div>
         <div className="my-4">
           <div className=" space-x-1 items-center">
@@ -45,8 +45,8 @@ The Project-based curriculum provides hands-on experience with JavaScript, HTML,
         <img className="w-full rounded-3xl h-56 shadow-md shadow-dark-blue" src={Ryan} alt="Ryan" /></div>
          <div className="flex space-x-5 items-baseline"> 
           <h1 className="mt-4 text-dark-blue text-2xl font-bold cursor-pointer">Ryan Lundy</h1>
-          <a href="https://www.linkedin.com/in/1ryanlundy/"><SiLinkedin /></a>
-          <a href="https://github.com/1ryanlundy"><SiGithub /></a>
+          <a href="https://www.linkedin.com/in/1ryanlundy/" target="_blank"><SiLinkedin /></a>
+          <a href="https://github.com/1ryanlundy" target="_blank"><SiGithub /></a>
         </div>
         <div className="my-4">
           <div className="flex space-x- items-center">
@@ -65,8 +65,8 @@ The Project-based curriculum provides hands-on experience with JavaScript, HTML,
         </div>
         <div className="flex space-x-5 items-baseline"> 
           <h1 className="mt-4 text-dark-blue text-2xl font-bold cursor-pointer">Florence Osei</h1>
-          <a href="https://www.linkedin.com/in/florence-ernestina-osei/"><SiLinkedin /></a>
-          <a href="https://github.com/pursuit-florence"><SiGithub /></a>
+          <a href="https://www.linkedin.com/in/florence-ernestina-osei/" target="_blank"><SiLinkedin /></a>
+          <a href="https://github.com/pursuit-florence" target="_blank"><SiGithub /></a>
         </div>
         <div className="my-4">
          
@@ -85,8 +85,8 @@ The Project-based curriculum provides hands-on experience with JavaScript, HTML,
         <img className="w-full rounded-3xl h-56 shadow-md shadow-dark-blue" src={Chris} alt="Chris" /> </div>
         <div className="flex space-x-5 items-baseline"> 
           <h1 className="mt-4 text-dark-blue text-2xl font-bold cursor-pointer">Christian Orlando</h1>
-          <a href="https://www.linkedin.com/in/christian-orlando-it/"><SiLinkedin /></a>
-          <a href="https://github.com/orlandochristian"><SiGithub /></a>
+          <a href="https://www.linkedin.com/in/christian-orlando-it/" target="_blank"><SiLinkedin /></a>
+          <a href="https://github.com/orlandochristian" target="_blank"><SiGithub /></a>
         </div>
         <div className="my-4">
           <div className=" space-x-1 items-center">
@@ -103,8 +103,8 @@ The Project-based curriculum provides hands-on experience with JavaScript, HTML,
         <img className="w-full rounded-3xl h-56 shadow-md shadow-dark-blue" src={Wilton} alt="Wilton" /> </div>
         <div className="flex space-x-5 items-baseline"> 
           <h1 className="mt-4 text-dark-blue text-2xl font-bold cursor-pointer">Wilton Camilo</h1>
-          <a href="https://www.linkedin.com/in/wilton-l-camilo-339b78130/"><SiLinkedin /></a>
-          <a href="https://github.com/WiltonCam"><SiGithub /></a>
+          <a href="https://www.linkedin.com/in/wilton-l-camilo-339b78130/" target="_blank"><SiLinkedin /></a>
+          <a href="https://github.com/WiltonCam" target="_blank"><SiGithub /></a>
         </div>
         <div className="my-4">
           <div className=" space-x-1 items-center">
